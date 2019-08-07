fd20548c9f92fcb1697053483650d3a0484c0a8f
Saliou DJALO <djalosaliou@gmail.com>
2019-08-07 19:38:01 +0400

    Initial Commit

---
+++ Collection View App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collection View App
+//
+//  Created by Saliou DJALO on 07/08/2019.
+//  Copyright © 2019 Saliou DJALO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
